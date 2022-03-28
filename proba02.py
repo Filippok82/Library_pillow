07b7b4abb4d821261d801ca9985f8915e3adfa27
Irina Filippova <irina.filippova.mail@yandex.ru>
2022-03-28 10:49:37 +0300

    Порпоцтональное изменение высоты и ширины

--- proba02.py
+++ proba02.py
@@ -14,9 +14,38 @@
 
 # Изменения размера изображения
 
+# from PIL import Image
+# img = Image.open('taksa.jpg')
+# img = img.resize((170, 100), Image.ANTIALIAS)
+# img.save('test_taksa.jpg')
+# img = Image.open('test_taksa.jpg')
+# img.show()
+
+#______________________________________
+
+# Изменение ширины с учетом пропорций для новой высоты изображения
+
+# from PIL import Image
+ 
+# tatras = Image.open("taksa.jpg")
+ 
+# width, height = tatras.size
+# new_width  = 580 # ширина
+# new_height = int(new_width * height / width)
+ 
+# tatras = tatras.resize((new_width, new_height), Image.ANTIALIAS)
+# tatras.show()
+
+#___________________________________________
+# Изменение высоты изображения, пропорционально обновляем и ширину
+
 from PIL import Image
-img = Image.open('taksa.jpg')
-img = img.resize((170, 100), Image.ANTIALIAS)
-img.save('test_taksa.jpg')
-img = Image.open('test_taksa.jpg')
-img.show()
+ 
+tatras = Image.open("taksa.jpg")
+ 
+width, height = tatras.size
+new_height = 680 # Высота
+new_width  = int(new_height * width / height)
+ 
+tatras = tatras.resize((new_width, new_height), Image.ANTIALIAS)
+tatras.show()
